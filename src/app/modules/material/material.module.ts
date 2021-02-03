c6112f4cde59e207f98fc47ddd5f71eed1edc474
sanchis <sanchis1992@gmail.com>
2021-02-03 23:02:23 +0100

    home interval slider

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const MATERIAL_MODULES = [
   MatToolbarModule,
   MatIconModule,
   BrowserAnimationsModule,
-  MatButtonModule
+  MatButtonModule,
+  MatProgressBarModule
 ];
 
 @NgModule({
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, of } from 'rxjs';
 
 @Component({
   selector: 'devaway-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public progress = 1;
+  public currentComponent = 0;
+  private readonly MAX_TIME_SECONDS = 30;
+
+
   constructor() { }
 
   ngOnInit() {
+    interval(1000).subscribe(() => this.componentSlider());
+  }
+
+  private componentSlider() {
+    this.progress += (100 / this.MAX_TIME_SECONDS);
+    console.log(this.progress);
+    if (this.progress >= 100) {
+      this.progress = 1;
+      this.currentComponent = this.currentComponent === 2 ? 0 : this.currentComponent + 1;
+    }
   }
 
 }
