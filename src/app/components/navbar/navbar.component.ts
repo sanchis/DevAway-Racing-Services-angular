68624c3a594e347a9161d34f107ed331419ff26f
sanchis <sanchis1992@gmail.com>
2021-02-07 17:15:14 +0100

    add effects

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { bounceInLeftOnEnterAnimation, rubberBandOnEnterAnimation } from 'angular-animations';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'devaway-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    rubberBandOnEnterAnimation(),
+    bounceInLeftOnEnterAnimation({
+      anchor: 'bounceOne',
+      delay: 0
+    }),
+    bounceInLeftOnEnterAnimation({
+      anchor: 'bounceTwo',
+      delay: 1000
+    }),
+    bounceInLeftOnEnterAnimation({
+      anchor: 'bounceThree',
+      delay: 2000
+    })
+  ]
 })
 export class NavbarComponent {
 
